8f1e31b240fb25d0a606eeb486e9332e94abda5e
ardra-ashok <ardraasok@yahoo.in>
2024-12-24 23:27:24 +1100

    refreshing basic python

--- pythonBasics.py
+++ pythonBasics.py
@@ -49,6 +49,61 @@ print(values)
 del values[0]
 print(values)
 
+# lists ,tuples and dictionaries
+
+# tuple - same as LIST datatype but immutable
+val = (1,2,"test",4.5)
+print(val[1])
+# val[2] = "hello"
+
+dic = {"a":2,4:"bcd","c":"hello"}
+print(dic[4])
+print(dic["c"])
+
+dict = {}
+dict["firstName"] = "Rahul"
+dict["lastName"] = "shetty"
+dict["gender"] = "Male"
+print(dict)
+print(dict["lastName"])
+
+# loops
+greeting = "Morning"
+
+if greeting == "Morning":
+    print("condition matches")
+    print("Second line")
+else:
+    print("Conditiion do not matches")
+
+# for loop
+obj = [2,3,4,5,6,7,8]
+for i in obj:
+    print(i*2)
+
+print("-----------")
+# range(i,j) -> i to j-1
+
+
+sum = 0
+for j in range(1,6):
+    sum +=j
+print(sum)
+
+#  - normal for loop ; for(int i=0;i<6;i++)
+# for j in range(1,6,2) - increment value is 2 here
+for k in range(1,15,5):
+    print(k)
+
+#   while loops
+i = 4
+while i>1:
+    if i!=3:
+        print(i)
+    i-=1
+
+
+
 
 
 
