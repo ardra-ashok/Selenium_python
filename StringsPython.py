312b8442ab6178191fb6d31720951685c90b458d
ardra-ashok <ardraasok@yahoo.in>
2024-12-27 21:23:07 +1100

    refreshing python basics

---
+++ StringsPython.py
@@ -0,0 +1,21 @@
+str = "TestAcademy.com"
+str1 = " Consulting firm"
+str3 = "Test"
+
+print(str[1])
+
+# Substring
+print(str[0:4])
+
+# Concatenation
+print(str+str1)
+
+print(str3 in str)
+print(str.split("."))
+print(str.split("c")[0])
+
+str_4 = " great "
+
+print(str_4.strip())
+print(str_4.lstrip())
+print(str_4.rstrip())
